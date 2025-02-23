584f78eed4f635ccbe6be1ae84cf234cc9dcd294
Tubsters <Tobias.henriksen2901@gmail.com>
2025-02-23 19:31:20 +0100

    ændringer til deploy

--- kanban-board/src/components/KanbanBoard.vue
+++ kanban-board/src/components/KanbanBoard.vue
@@ -2,7 +2,7 @@
 import { defineComponent } from 'vue'
 import KanbanColumn from './KanbanColumn.vue'
 import { useKanban } from '../composables/useKanban'
-import type { Card, Column } from '../composables/useKanban'
+import type { Card,} from '../composables/useKanban'
 
 export default defineComponent({
   name: 'KanbanBoard',
--- kanban-board/vite.config.ts
+++ kanban-board/vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // Sørger for korrekt base-path
+  server: {
+    port: 5173, // Kan være valgfrit
+  }
 })
